1078fa067d45c8f86dbd6b23a90018b15d0ad182
BlueSeaLidar <60593455+BlueSeaLidar@users.noreply.github.com>
2021-05-13 18:09:37 +0800

    timestamp

--- src/parser.cpp
+++ src/parser.cpp
@@ -262,15 +262,15 @@ void DecTimestamp(uint32_t ts, uint32_t* ts2)
 	gettimeofday(&tv, NULL);
 
 	uint32_t sec = tv.tv_sec % 3600;
-	if (sec < 5 && ts/10000 > 3595)
+	if (sec < 5 && ts/1000 > 3595)
 	{
-		ts2[0] = (tv.tv_sec / 3600 -1) * 3600 + ts/10000;
+		ts2[0] = (tv.tv_sec / 3600 -1) * 3600 + ts/1000;
 	}
 	else {
-		ts2[0] = (tv.tv_sec / 3600) * 3600 + ts/10000;
+		ts2[0] = (tv.tv_sec / 3600) * 3600 + ts/1000;
 	}
 
-	ts2[1] = (ts%10000)*10000;
+	ts2[1] = (ts%1000)*1000;
 }
 
 static RawData* PackFanData(FanSegment* seg)
